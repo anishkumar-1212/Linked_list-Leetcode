0d0953618bbb948f42168019f99d41e3c3dee3da
Anish Kumar <patelanishh1212@gmail.com>
2026-08-01 01:52:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (89.76%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> st;
-
-        for (char ch : s) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
+    bool isValid(string str) {
+        stack<char> st; // list of stack
+        for (int i = 0; i < str.size(); i++) {
+            // for opening check to ush to the stack
+            if (str[i] == '(' || str[i] == '{' || str[i]=='[') {
+                st.push(str[i]);
             } else {
-                if (st.empty()) {
-                    return false;
+                    if (st.size() == 0) {
+                        return false;
+                    }
+                    //for closing checkk up 
+                    if ((st.top() == '(' && str[i] == ')') ||
+                        (st.top() == '{' && str[i] == '}') ||
+                        (st.top() == '[' && str[i] == ']')) {
+                        st.pop();
+                    } else {
+                        return false;  // no match and its invalid paranthesis
+                    }
                 }
-
-                char top = st.top();
-                st.pop();
-
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
-                    return false;
-                }
-            }
         }
-
-        return st.empty();
+        return st.size()==0;
     }
 };
