c049d18d538675fc3ec1ee8355543fc05af53520
Anish Kumar <patelanishh1212@gmail.com>
2026-09-02 12:23:50 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (44.63%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int count = 0;
-        int j=0;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; i++) {
-            if(nums[i]==nums[j]){
-                count++;
-            }else{
-                j++;
-            }
-            if(count>n/2){
-                return nums[i];
-            }
-        }
-        return -1;
+        return nums[nums.size() / 2]; // The majority element always crosses the midpoint
     }
 };
